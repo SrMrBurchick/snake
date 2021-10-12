6fa7150ca604e9ce5dd321a213e5e1667019f355
Serhii Bura <serhiibura@gmail.com>
2021-10-12 16:00:05 +0300

    Add map drawing
    Signed-off-by: Serhii Bura <serhiibura@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,78 @@
+extern crate termion;
+
+static G_BORDER: &'static str = "*";
+static G_EMPTY: &'static str = " ";
+static G_APPLE: &'static str = "@";
+static G_SNAKE: &'static str = "#";
+
+#[derive(PartialEq, Debug, Clone, Copy)]
+enum MapObjects {
+    Empty = 0,
+    Border = 1,
+    Apple = 2,
+    Snake = 3,
+    MapObjectsCount,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Object {
+    x: u32,
+    y: u32,
+    objectType: MapObjects,
+    changed: bool
+}
+impl Object {
+    fn new() -> Object {
+        Object {x: 0, y: 0, objectType: MapObjects::Empty, changed: true}
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut map = [[Object::new(); 60]; 40];
+
+    initMap(&mut map);
+
+    drawMap(&map);
+}
+
+fn initMap(map: &mut [[Object; 60]; 40]) {
+    for i in 0..40 {
+        for j in 0..60 {
+            if (i == 0 || i == 39) || (j == 0 || j == 59) {
+                map[i][j].objectType = MapObjects::Border;
+                map[i][j].x = j as u32;
+                map[i][j].y = i as u32;
+                map[i][j].changed = true;
+            } else {
+                map[i][j].objectType = MapObjects::Empty;
+                map[i][j].x = j as u32;
+                map[i][j].y = i as u32;
+                map[i][j].changed = true;
+            }
+        }
+    }
+}
+
+fn drawObject(object: &Object) {
+    if true == object.changed {
+        print!("{}", termion::cursor::Goto((object.x + 1) as u16, (object.y + 1) as u16));
+        match object.objectType {
+            MapObjects::Empty => print!("{}", G_EMPTY),
+            MapObjects::Border => print!("{}", G_BORDER),
+            MapObjects::Apple => print!("{}", G_APPLE),
+            MapObjects::Snake => print!("{}", G_SNAKE),
+            _ => print!("")
+        }
+
+    }
+}
+
+fn drawMap(map: &[[Object; 60]; 40]) {
+    print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+    for (i, row) in map.iter().enumerate() {
+        for (j, col) in row.iter().enumerate() {
+            drawObject(col);
+        }
+        print!("\n");
+    }
 }
